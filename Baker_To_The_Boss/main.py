5adf6de361b22e3d750aebcdc4172d6a0f6f16c1
ZackOhLantern <Z.Cuervo@live.com>
2023-01-13 22:07:49 -0800

    created main

--- Baker_To_The_Boss/main.py
+++ Baker_To_The_Boss/main.py
@@ -1,4 +1,18 @@
 import pygame
 
 WIDTH, HEIGHT = 800, 600
-WIN = pygame.display.set_mode(())
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+
+def main():
+
+    run = True
+    while run:
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+
+    pygame.quit()
+
+    if __name__ == "__main__":
+        main()
